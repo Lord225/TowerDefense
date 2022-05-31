a73b96b3ad4ddef19805ccbea527d4f3c61ace1e
Lord225 <zlotymaciej@gmail.com>
2022-05-31 19:29:13 +0200

    Base structure

--- core/src/com/mygdx/game/Map/Map.java
+++ core/src/com/mygdx/game/Map/Map.java
@@ -1,11 +1,18 @@
 package com.mygdx.game.Map;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.mygdx.game.Enemy.Enemy;
+
+import java.util.Vector;
 
 public class Map extends Sprite
 {
     Tile[][] tiles;
+    Vector<Enemy> enemies = new Vector<>();
 
+    void update()
+    {
 
+    }
 
 }
