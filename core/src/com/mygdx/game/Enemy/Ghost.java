89f33c7a6d551dd6eae7902d5a36bb129c1a043a
xDekann <oozog255@gmail.com>
2022-05-31 19:51:37 +0200

    Potwory poczatek

---
+++ core/src/com/mygdx/game/Enemy/Ghost.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.Enemy;
+
+public class Ghost extends Enemy{
+
+    public Ghost(double hp, double speed){
+        this.health = hp;
+        this.speed = speed;
+    }
+    @Override
+    void updateHealth(double damage){
+        this.health -= getHealth();
+    };
+    @Override
+    void move(){};
+    @Override
+    double getHealth(){
+        return this.health;
+    };
+    @Override
+    void setHealth(double newHp){
+        this.health = newHp;
+    };
+    @Override
+    double getSpeed(){
+        return this.speed;
+    };
+    @Override
+    void setSpeed(int newVal){
+        this.speed = this.speed - newVal;
+    };
+}
