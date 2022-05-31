f04a74396b1ab5b38abe09ba9ea1f971a32fa9e0
DESKTOP-SJHKUSQ\Brzydkionjest <king@pendracki@gmail.com>
2022-05-31 20:28:05 +0200

    Added widthXheight

--- core/src/Miscellaneous/Resources.java
+++ core/src/Miscellaneous/Resources.java
@@ -5,9 +5,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Resources {
-    public static Sprite ghost_sprite= new Sprite(new Texture(Gdx.files.internal("textures/enemy/ghost.png")));
-    public static Sprite arrow_sprite= new Sprite(new Texture(Gdx.files.internal("textures/entity/arrow.png")));
-    public static Sprite dirt_sprite= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt.png")));
-    public static Sprite dirt_green_sprite= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt_green.png")));
-    public static Sprite tower_sprite = new Sprite(new Texture(Gdx.files.internal("textures/tiles/tower.png")));
+    public static Sprite ghost_sprite= new Sprite(new Texture(Gdx.files.internal("textures/enemy/ghost.png")),32,32);
+    public static Sprite arrow_sprite= new Sprite(new Texture(Gdx.files.internal("textures/entity/arrow.png")),32,32);
+    public static Sprite dirt_sprite= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt.png")),32,32);
+    public static Sprite dirt_green_sprite= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt_green.png")),32,32);
+    public static Sprite tower_sprite = new Sprite(new Texture(Gdx.files.internal("textures/tiles/tower.png")),32,32);
 }
