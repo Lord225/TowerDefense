ddfe2f195e7dc3d12bb921a17c3087ec8b38256d
DESKTOP-SJHKUSQ\Brzydkionjest <king@pendracki@gmail.com>
2022-05-31 20:13:42 +0200

    Changed textures to sprite Added tower texture

--- core/src/Miscellaneous/Resources.java
+++ core/src/Miscellaneous/Resources.java
@@ -5,10 +5,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Resources {
-    public static Sprite ghost_texture= new Sprite(new Texture(Gdx.files.internal("textures/enemy/ghost.png")));
-    public static Sprite arrow_texture= new Sprite(new Texture(Gdx.files.internal("textures/entity/arrow.png")));
-    public static Sprite dirt_texture= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt.png")));
-    public static Sprite dirt_green_texture= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt_green.png")));
-
-
+    public static Sprite ghost_sprite= new Sprite(new Texture(Gdx.files.internal("textures/enemy/ghost.png")));
+    public static Sprite arrow_sprite= new Sprite(new Texture(Gdx.files.internal("textures/entity/arrow.png")));
+    public static Sprite dirt_sprite= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt.png")));
+    public static Sprite dirt_green_sprite= new Sprite(new Texture(Gdx.files.internal("textures/tiles/dirt_green.png")));
+    public static Sprite tower_sprite = new Sprite(new Texture(Gdx.files.internal("textures/tiles/tower.png")));
 }
