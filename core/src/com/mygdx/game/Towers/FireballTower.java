e90f825eea76a927a5b88040c6f8951fec9b8b54
xDekann <oozog255@gmail.com>
2022-06-15 12:57:34 +0200

    castle overlaying green block

--- core/src/com/mygdx/game/Towers/FireballTower.java
+++ core/src/com/mygdx/game/Towers/FireballTower.java
@@ -23,47 +23,10 @@ public class FireballTower extends Building
         this.skin.setColor(0f,255.0f,0f,1f);
         this.fireballrange = fireballrange;
     }
-
-    @Override
-    public void update_enemies(Entity[] enemies_in_range)
-    {
-        if(enemies_in_range.length == 0)
-            return;
-
-        if(currentCooldown >= shootingCooldown)
-        {
-            var target = enemies_in_range[rnd.nextInt(enemies_in_range.length)];
-
-            if (target instanceof Enemy enemy) {
-                FireBall.emmit(enemy, tile, fireballrange);
-                currentCooldown = 0;
-            }
-        }
-    }
-
     @Override
     public void update()
     {
         currentCooldown += Gdx.graphics.getDeltaTime();
     }
 
-    @Override
-    public float getRange() {
-        return this.range;
-    }
-
-    @Override
-    public void setRange(float range) {
-        this.range = range;
-    }
-
-    @Override
-    public float getCooldown() {
-        return this.shootingCooldown;
-    }
-
-    @Override
-    public void setCooldown(float cooldown) {
-        this.shootingCooldown = cooldown;
-    }
 }
